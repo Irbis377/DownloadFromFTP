39da69374e238ada75311d713537c282021abb89
Irina <irbis377@gmail.com>
2019-08-26 16:43:16 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WF_DownloadFTP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_serverUrl = new System.Windows.Forms.TextBox();
+            this.pb_download = new System.Windows.Forms.ProgressBar();
+            this.bt_download = new System.Windows.Forms.Button();
+            this.lb_serverUrl = new System.Windows.Forms.Label();
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.lb_login = new System.Windows.Forms.Label();
+            this.lb_pass = new System.Windows.Forms.Label();
+            this.lb_count = new System.Windows.Forms.Label();
+            this.sfd_downloadedFile = new System.Windows.Forms.SaveFileDialog();
+            this.cb_auth = new System.Windows.Forms.CheckBox();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.bgw_download = new System.ComponentModel.BackgroundWorker();
+            this.lb_speed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_serverUrl
+            // 
+            this.tb_serverUrl.Location = new System.Drawing.Point(10, 31);
+            this.tb_serverUrl.Name = "tb_serverUrl";
+            this.tb_serverUrl.Size = new System.Drawing.Size(447, 20);
+            this.tb_serverUrl.TabIndex = 0;
+            // 
+            // pb_download
+            // 
+            this.pb_download.Location = new System.Drawing.Point(10, 138);
+            this.pb_download.Name = "pb_download";
+            this.pb_download.Size = new System.Drawing.Size(447, 23);
+            this.pb_download.TabIndex = 7;
+            // 
+            // bt_download
+            // 
+            this.bt_download.Location = new System.Drawing.Point(463, 31);
+            this.bt_download.Name = "bt_download";
+            this.bt_download.Size = new System.Drawing.Size(116, 108);
+            this.bt_download.TabIndex = 3;
+            this.bt_download.Text = "Go!";
+            this.bt_download.UseVisualStyleBackColor = true;
+            this.bt_download.Click += new System.EventHandler(this.bt_download_Click);
+            // 
+            // lb_serverUrl
+            // 
+            this.lb_serverUrl.AutoSize = true;
+            this.lb_serverUrl.Location = new System.Drawing.Point(7, 9);
+            this.lb_serverUrl.Name = "lb_serverUrl";
+            this.lb_serverUrl.Size = new System.Drawing.Size(60, 13);
+            this.lb_serverUrl.TabIndex = 4;
+            this.lb_serverUrl.Text = "Path to file:";
+            // 
+            // tb_login
+            // 
+            this.tb_login.Location = new System.Drawing.Point(73, 57);
+            this.tb_login.Name = "tb_login";
+            this.tb_login.Size = new System.Drawing.Size(169, 20);
+            this.tb_login.TabIndex = 1;
+            this.tb_login.Text = "login";
+            this.tb_login.Visible = false;
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.Location = new System.Drawing.Point(73, 80);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.Size = new System.Drawing.Size(169, 20);
+            this.tb_pass.TabIndex = 2;
+            this.tb_pass.Text = "password";
+            this.tb_pass.UseSystemPasswordChar = true;
+            this.tb_pass.Visible = false;
+            // 
+            // lb_login
+            // 
+            this.lb_login.AutoSize = true;
+            this.lb_login.Location = new System.Drawing.Point(7, 60);
+            this.lb_login.Name = "lb_login";
+            this.lb_login.Size = new System.Drawing.Size(58, 13);
+            this.lb_login.TabIndex = 5;
+            this.lb_login.Text = "Username:";
+            this.lb_login.Visible = false;
+            // 
+            // lb_pass
+            // 
+            this.lb_pass.AutoSize = true;
+            this.lb_pass.Location = new System.Drawing.Point(7, 83);
+            this.lb_pass.Name = "lb_pass";
+            this.lb_pass.Size = new System.Drawing.Size(56, 13);
+            this.lb_pass.TabIndex = 6;
+            this.lb_pass.Text = "Password:";
+            this.lb_pass.Visible = false;
+            // 
+            // lb_count
+            // 
+            this.lb_count.AutoSize = true;
+            this.lb_count.Location = new System.Drawing.Point(265, 100);
+            this.lb_count.Name = "lb_count";
+            this.lb_count.Size = new System.Drawing.Size(32, 13);
+            this.lb_count.TabIndex = 9;
+            this.lb_count.Text = "bytes";
+            // 
+            // cb_auth
+            // 
+            this.cb_auth.AutoSize = true;
+            this.cb_auth.Location = new System.Drawing.Point(265, 57);
+            this.cb_auth.Name = "cb_auth";
+            this.cb_auth.Size = new System.Drawing.Size(131, 17);
+            this.cb_auth.TabIndex = 10;
+            this.cb_auth.Text = "Need authentificaton?";
+            this.cb_auth.UseVisualStyleBackColor = true;
+            this.cb_auth.CheckedChanged += new System.EventHandler(this.cb_auth_CheckedChanged);
+            // 
+            // lb_time
+            // 
+            this.lb_time.AutoSize = true;
+            this.lb_time.Location = new System.Drawing.Point(265, 83);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(26, 13);
+            this.lb_time.TabIndex = 11;
+            this.lb_time.Text = "time";
+            // 
+            // bgw_download
+            // 
+            this.bgw_download.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_download_DoWork);
+            // 
+            // lb_speed
+            // 
+            this.lb_speed.AutoSize = true;
+            this.lb_speed.Location = new System.Drawing.Point(265, 117);
+            this.lb_speed.Name = "lb_speed";
+            this.lb_speed.Size = new System.Drawing.Size(36, 13);
+            this.lb_speed.TabIndex = 12;
+            this.lb_speed.Text = "speed";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 173);
+            this.Controls.Add(this.lb_speed);
+            this.Controls.Add(this.lb_time);
+            this.Controls.Add(this.cb_auth);
+            this.Controls.Add(this.lb_count);
+            this.Controls.Add(this.lb_pass);
+            this.Controls.Add(this.lb_login);
+            this.Controls.Add(this.tb_pass);
+            this.Controls.Add(this.tb_login);
+            this.Controls.Add(this.lb_serverUrl);
+            this.Controls.Add(this.bt_download);
+            this.Controls.Add(this.pb_download);
+            this.Controls.Add(this.tb_serverUrl);
+            this.Name = "MainForm";
+            this.Text = "Download from FTP";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_serverUrl;
+        private System.Windows.Forms.ProgressBar pb_download;
+        private System.Windows.Forms.Button bt_download;
+        private System.Windows.Forms.Label lb_serverUrl;
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.TextBox tb_pass;
+        private System.Windows.Forms.Label lb_login;
+        private System.Windows.Forms.Label lb_pass;
+        private System.Windows.Forms.Label lb_count;
+        private System.Windows.Forms.SaveFileDialog sfd_downloadedFile;
+        private System.Windows.Forms.CheckBox cb_auth;
+        private System.Windows.Forms.Label lb_time;
+        private System.ComponentModel.BackgroundWorker bgw_download;
+        private System.Windows.Forms.Label lb_speed;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WF_DownloadFTP
+{
+    public partial class MainForm : Form
+    {
+        private string RemoteFtpPath;
+        private string LocalDestinationPath;
+        private bool auth = false;
+        private long size = 0;
+        private const int buffsize = 1024;
+        public delegate void ChangeTextSafe(TimeSpan time, int position, int percent, double speed);
+        public delegate void DownloadComplete();
+      
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_download_Click(object sender, EventArgs e)
+        {
+
+            lb_count.Text = "";
+            lb_time.Text = "";
+            lb_speed.Text = "";
+            //get address of file 
+            RemoteFtpPath = tb_serverUrl.Text;
+
+            if (string.IsNullOrEmpty(RemoteFtpPath))
+            {
+                MessageBox.Show("Check the path!");
+                return;
+            }
+
+            //ask where need to save this file 
+            sfd_downloadedFile  = new SaveFileDialog();
+            sfd_downloadedFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            sfd_downloadedFile.FilterIndex = 2;
+            sfd_downloadedFile.RestoreDirectory = true;
+            string[] name = RemoteFtpPath.TrimEnd().Split('/');
+            sfd_downloadedFile.FileName = name[name.Length - 1];
+
+            if (sfd_downloadedFile.ShowDialog() != DialogResult.OK)
+            {
+                  return; 
+            }
+
+            LocalDestinationPath = sfd_downloadedFile.FileName;
+            
+            if (string.IsNullOrEmpty(LocalDestinationPath))
+            {
+                MessageBox.Show("Check the LocalDestinationPath!");
+                return;
+            }
+
+
+            if (auth)
+            {
+                if (string.IsNullOrEmpty(tb_login.Text) || string.IsNullOrEmpty(tb_pass.Text))
+                {
+                    MessageBox.Show("Check your username and password!");
+                    return;
+                }
+            }
+
+            this.Text = "Trying to get file size...";
+
+            // Query size of the file to be downloaded
+            WebRequest sizeRequest = WebRequest.Create(RemoteFtpPath);
+            if (auth)
+            {
+                sizeRequest.Credentials = new NetworkCredential(tb_login.Text, tb_pass.Text);
+            }
+            sizeRequest.Method = WebRequestMethods.Ftp.GetFileSize;
+            try
+            {
+                size = sizeRequest.GetResponse().ContentLength;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Text = "Download from FTP";
+                return;
+            }
+           
+            //set maximu value of progressbar
+            pb_download.Invoke((MethodInvoker)(() => pb_download.Maximum = (int)size));
+
+            pb_download.Visible = true;
+            lb_count.Visible = true;
+            bt_download.Enabled = false;
+
+            try
+            {
+                //start async download 
+                bgw_download.RunWorkerAsync();
+
+              
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                bt_download.Enabled = true;
+            }
+
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lb_count.Text = "";
+            lb_time.Text = "";
+            lb_speed.Text = "";
+            pb_download.Visible = false;
+
+
+        }
+
+        private void cb_auth_CheckedChanged(object sender, EventArgs e)
+        {
+            //show textboxes for auth
+            if (cb_auth.Checked == true)
+            {
+                auth = true;
+                lb_login.Visible = true;
+                lb_pass.Visible = true;
+                tb_login.Visible = true;
+                tb_pass.Visible = true;
+            }
+            else  //hide textboxes for auth
+            {
+                auth = false; 
+                lb_login.Visible = false;
+                lb_pass.Visible = false;
+                tb_login.Visible = false;
+                tb_pass.Visible = false;
+
+                tb_login.Text = string.Empty;
+                tb_pass.Text = string.Empty;
+            }
+        }
+
+        private void bgw_download_DoWork(object sender, DoWorkEventArgs e)
+        { 
+            //create new delegate to show progress on label and for the end of downloading
+            ChangeTextSafe safedelegate = ChangeTextOnLabels;
+            DownloadComplete downloadComplete = EndOfDownload;
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(RemoteFtpPath);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            //if we need authentfication - set credentials
+            if (auth)
+            {
+                request.Credentials = new NetworkCredential(tb_login.Text, tb_pass.Text);
+            }
+
+            //need for show current speed 
+            Stopwatch speedTimer = new Stopwatch();
+            //need for show estimated time 
+            TimeSpan elapsedTime;
+            double currentSpeed = -1;
+            double recivedBytes = 0; 
+            
+            using (Stream ftpStream = request.GetResponse().GetResponseStream())
+            using (Stream fileStream = File.Create(LocalDestinationPath))
+            {
+                byte[] buffer = new byte[buffsize];
+                int read;
+                DateTime started = DateTime.Now;
+
+                speedTimer.Start();
+                while ((read = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                   
+                    fileStream.Write(buffer, 0, read);
+                    recivedBytes += read;
+
+                    int position = (int)fileStream.Position;
+                    //show progress on progressbar
+                    pb_download.Invoke((MethodInvoker)(() => pb_download.Value = position));
+                    //calculate transfer rate 
+                    currentSpeed = position / 1024d / speedTimer.Elapsed.TotalSeconds; //kb/sec
+                    
+                    //calculate estimated time
+                    elapsedTime = DateTime.Now - started;
+                    TimeSpan estimatedTime = TimeSpan.FromSeconds((double)(size - recivedBytes) /*bytes left */ * elapsedTime.TotalSeconds / (double)recivedBytes);
+                    //calculate how many percent we've already got
+                    short percent = (short)((position * 100)/size);
+                    //call the delegate to refresh progress on labels
+                    this.Invoke(safedelegate, estimatedTime, position, percent, currentSpeed);
+                }
+
+                //we've received file
+                speedTimer.Stop();
+                this.Invoke(downloadComplete);
+            }
+        }
+
+        public void ChangeTextOnLabels(TimeSpan estTime, int position, int percent, double speed)
+        {
+            lb_time.Text = estTime.ToString(); 
+            lb_count.Text = position.ToString() + "/" + size.ToString() + " bytes" + " ("+percent.ToString()+"%)";
+            lb_speed.Text = String.Format("{0} kb/s", speed.ToString("0.##")); 
+            
+        }
+
+        public void EndOfDownload()
+        {
+            lb_time.Text = "Download finished!";
+            lb_speed.Text = "";
+            pb_download.Visible = false;
+            bt_download.Enabled = true;
+
+            this.Text = "Download from FTP";
+
+        }
+    }
+}
